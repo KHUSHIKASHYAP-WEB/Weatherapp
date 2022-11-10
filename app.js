d3c5ce7f9742bda70e432517d7c9c5cdf8b3775c
Khushi Kashyap <khushikashyap126@gmail.com>
2022-11-11 00:48:54 +0530

    fc

--- app.js
+++ app.js
@@ -170,3 +170,21 @@ function showWeatherData(data) {
   });
   otherday.innerHTML = otherdayForecast;
 }
+document.addEventListener("DOMContentLoaded", () => {
+  const selectDrop = document.querySelector("#search-value");
+  fetch("https://restcountries.com/v3.1/all")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      let output = "";
+      data.forEach((country) => {
+        output += `<option>${country.name.common}</option>`;
+        console.log(country.callingCodes);
+      });
+      selectDrop.innerHTML = output;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
